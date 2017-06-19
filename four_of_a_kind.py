3bc8266aea300144b07f4d12134eaab581c7c9cc
Kelle, Dallas <dallas.kelle@intel.com>
2017-06-19 15:05:14 -0700

    finished rough draft of three of a kind, and four of a kind

--- four_of_a_kind.py
+++ four_of_a_kind.py
@@ -1,15 +1,15 @@
 from lower_section_hand import lower_section_hand
 
-class three_of_a_kind(lower_section_hand):
+class four_of_a_kind(lower_section_hand):
 
 
   def get_points(self, dice):
-    if max(self.get_occurance_list(dice)) >= 3:
+    if max(self.get_occurance_list(dice)) >= 4:
       return sum(dice)
     return 0
 
   def get_hand_name(self):
-    return "three of a kind"
+    return "four of a kind"
 
   def get_weight(self, dice):
     debug = False 
@@ -23,21 +23,21 @@ class three_of_a_kind(lower_section_hand):
     common_roll = occurance_list.index(occurances)
     if debug: print 'have mostly ' + str(common_roll)
 
-    #which dice would we want to re roll if we are going for three of a kind?
+    #which dice would we want to re roll if we are going for four of a kind?
     reroll = [i for i, x in enumerate(dice) if not x == common_roll] 
 
     if debug:
-      print 'if going for 3 of a kind, would reroll ' + str(reroll)
+      print 'if going for 4 of a kind, would reroll ' + str(reroll)
 
     #How many dice are we wanting to re-roll?
     bad_die = len(reroll)
 
     #
-    #What is the chance we get a three of a kind if we re-roll these dice?
+    #What is the chance we get a four of a kind if we re-roll these dice?
     #
 
-    #3 - 'the number of good we already have' is the number of 'good' rolls we need to get a 3 of a kind
-    good_still_needed = max(0, 3-occurances)
+    #4 - 'the number of good we already have' is the number of 'good' rolls we need to get a 4 of a kind
+    good_still_needed = max(0, 4-occurances)
    
     #bad_die - 'good_still_needed' is how many 'bad rolls' we can allow and still get the three of a kind
     bad_allowed = bad_die - good_still_needed
@@ -47,8 +47,6 @@ class three_of_a_kind(lower_section_hand):
     chance_of_bad = (1.0-self.sixth) ** bad_allowed
     chance_of_both = chance_of_good * chance_of_bad
 
-    if debug: print 'sixth: ' 
-    if debug: print self.sixth
     if debug: print 'chance of good: ' + str(chance_of_good)
     if debug: print 'chance of bad: ' + str(chance_of_bad)
     if debug: print 'chance of both: ' + str(chance_of_both)
@@ -63,8 +61,8 @@ class three_of_a_kind(lower_section_hand):
 
     if debug: print 'total prob: ' + str(probability)
 
-    #What would our score be if we did get the three of a kind?
-    total_score = (common_roll * 3) +  6 #6 because on average let's assume our other dice are 3
+    #What would our score be if we did get the four of a kind?
+    total_score = (common_roll * 4) +  3 #3 because on average let's assume the other dice is a three
 
     if debug: print 'total score would be ' + str(total_score)
     if debug: print 'total weight would be ' + str((probability*total_score))
--- upper_section_hand.py
+++ upper_section_hand.py
@@ -4,7 +4,7 @@ class upper_section_hand(hand):
 
   def __init__(self, value):
     self.taken = False
-    self.sixth = 1/6
+    self.sixth = 1.0/6.0
     self.value = value
     self.hand_name_dict = {1:"ones", 2:"twos", 3:"threes", 4:"fours", 5:"fives", 6:"sixes"}
 
@@ -12,20 +12,29 @@ class upper_section_hand(hand):
     return self.value * dice.count(self.value)
 
   def get_weight(self, dice):
+    debug = False 
+
+    if debug: print dice
+
     #What would our score be if we took this hand without re rolling?
     cur = self.get_points(dice)
+    if debug: print 'if we go for ' + self.get_hand_name() + ' we are guarenteed ' +str(cur)
 
     #Which dice would we re roll if we wanted to go for this hand?
     reroll = [i for i, x in enumerate(dice) if not x == self.value]
 
     #How many dice are "bad" rolls?
-    bad_die = len(reroll)
+    bad_die = len(reroll) + 0.0
+    if debug: print 'We have ' + str(bad_die) + ' that would not help this hand'
 
     #How many "good rolls" do we expect if we reroll all of our "bad" dice?
-    expected_good = bad_die * self.sixth
+    expected_good = (bad_die+0.0) * self.sixth
+    if debug: print 'out of our bad die, we exped to roll this many good die: ' +str(expected_good)
+    if debug: print self.sixth
 
     #what is our expected score?
-    return (cur + expected_good) * self.value, reroll
+    if debug: print 'expected score for ' + self.get_hand_name() + ' is ' + str((cur + expected_good*self.value))
+    return cur + (expected_good * self.value), reroll
 
   def get_hand_name(self):
     return self.hand_name_dict[self.value] if self.value in self.hand_name_dict else "NONE"
